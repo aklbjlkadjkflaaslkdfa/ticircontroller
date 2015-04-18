98949df3725b44a1d54f65793f3053dc7e91440e
xjiang@mbp <xjiang@mobvoi.com>
2015-04-18 17:29:22 +0800

    now can detect commandgit status

---
+++ arduino_server/wifly_http/wifly_http.ino
@@ -0,0 +1,128 @@
+
+
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <WiFly.h>
+#include "HTTPClient.h"
+
+#define SSID      "Xiaomi_JXITC"
+#define KEY       "xjiang318"
+// WIFLY_AUTH_OPEN / WIFLY_AUTH_WPA1 / WIFLY_AUTH_WPA1_2 / WIFLY_AUTH_WPA2_PSK
+#define AUTH      WIFLY_AUTH_WPA2_PSK
+
+#define HTTP_GET_URL "http://192.168.31.184:5000/"
+#define HTTP_POST_URL "http://httpbin.org/post"
+#define HTTP_POST_DATA "Hello world!"
+
+// Pins' connection
+// Arduino       WiFly
+//  2    <---->    TX
+//  3    <---->    RX
+SoftwareSerial uart(2, 3);
+WiFly wifly(uart);
+HTTPClient http;
+char get;
+
+// Xiao's command findvars
+char *line = new char[1000];
+int linePos = 0;
+
+const char *CMD_PREFIX = "CMD\0";
+const int CMD_PREFIX_LEN = 3;
+int cmdPrefixIdx = 0;
+
+/*
+ * Find command in Stream
+ */
+int getCommand(char c) {
+  if (cmdPrefixIdx == CMD_PREFIX_LEN) {
+    cmdPrefixIdx = 0;  // reset
+    return c - '0';
+  }
+
+  if (CMD_PREFIX[cmdPrefixIdx] == c) {
+    if (cmdPrefixIdx < CMD_PREFIX_LEN) {
+      // 0,1
+      cmdPrefixIdx++;
+    }
+  } else {
+    cmdPrefixIdx = 0;
+    return -1;
+  }
+
+  return -1;
+}
+
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("------- WIFLY HTTP --------");
+
+  const char *cmdPrefix = "Hello";
+  int iCur = 0;
+
+  uart.begin(9600);         // WiFly UART Baud Rate: 9600
+  // Wait WiFly to init
+  //  delay(3000);
+
+  // check if WiFly is associated with AP(SSID)
+  if (!wifly.isAssociated(SSID)) {
+    while (!wifly.join(SSID, KEY, AUTH)) {
+      Serial.println("Failed to join " SSID);
+      Serial.println("Wait 0.1 second and try again...");
+      delay(100);
+    }
+
+    wifly.save();    // save configuration,
+  }
+
+  wifly.sendCommand("set com remote 0\r");
+
+  Serial.println("\r\nTry to get url - " HTTP_GET_URL);
+  Serial.println("------------------------------");
+  while (http.get(HTTP_GET_URL, 10000) < 0) {
+  }
+
+  while (wifly.receive((uint8_t *)&get, 1, 1000) == 1) {
+    Serial.print(get);
+    if (getCommand(get) != -1) {
+      Serial.print("Detected command on char: ");
+      Serial.println(get);
+    }
+
+  }
+
+  Serial.println("\r\n\r\nTry to post data to url - " HTTP_POST_URL);
+  Serial.println("-------------------------------");
+  while (http.post(HTTP_POST_URL, HTTP_POST_DATA, 10000) < 0) {
+  }
+  while (wifly.receive((uint8_t *)&get, 1, 1000) == 1) {
+    Serial.print(get);
+  }
+
+  if (wifly.commandMode()) {
+    Serial.println("\r\n\r\nEnter command mode. Send \"exit\"(with \\r) to exit command mode");
+  }
+}
+
+
+void loop() {
+  int c;
+  while (wifly.available()) {
+    c = wifly.read();
+    if (c > 0) {
+      Serial.write((char)c);
+    }
+  }
+
+  while (Serial.available()) {
+    c = Serial.read();
+    if (c >= 0) {
+      wifly.write((char)c);
+    }
+  }
+}
+
+
+
